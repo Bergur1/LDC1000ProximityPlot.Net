3784d3709616a99832dfff5736c39f51fa74f7cc
Bergur Gudbergsson <bgudbergsson@saturnsys.com>
2014-12-12 16:58:56 -0600

    More in line with traditional MVC projects, the viewModel is replaced by controller, UI and polling now run on seperate threads. Now using Exocortex for FFT. In progress: FFT stuffs

---
+++ LDC1000ProximityPlot/Controller.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Windows.Input;
+using OxyPlot;
+using OxyPlot.Series;
+using Exocortex;
+
+namespace LDC1000ProximityPlot
+{
+    public class Controller : INotifyPropertyChanged
+    {
+        ProximityReaderThread pollingThread;
+
+        #region constructor stuff
+        public PlotModel Model { get; private set; }
+        LineSeries MainSeries { get; set; }
+        public Controller()
+        {
+            // Create the plot model
+            var tmp = new PlotModel { Title = "Proximity" };
+            // Create two line series (markers are hidden by default)
+            MainSeries = new LineSeries { MarkerType = MarkerType.None };
+            // Add the series to the plot model
+            tmp.Series.Add(MainSeries);
+            // Axes are created automatically if they are not defined
+
+            // Set the Model property, the INotifyPropertyChanged event will make the WPF Plot control update its content
+            this.Model = tmp;
+            FFTSampleSize = 1024;
+        }
+        #endregion
+
+        private ICommand _readCommand; 
+        public ICommand ReadCommand
+        {
+            get
+            {
+                if (_readCommand == null)
+                {
+                    _readCommand = new RelayCommand(
+                        param => this.StartSensor()
+                    );
+                }
+                return _readCommand;
+            }
+        }
+
+        private ICommand _executeFFTCommand;
+        public ICommand ExecuteFFTCommand
+        {
+            get
+            {
+                if (_executeFFTCommand == null)
+                {
+                    _executeFFTCommand = new RelayCommand(
+                        param => this.FFT()
+                            );
+                }
+                return _executeFFTCommand;
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string property)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        private string _proxyButtonText = "Start";
+        public string ProxyButtonText
+        {
+            get { return _proxyButtonText; }
+            set
+            {
+                _proxyButtonText = value;
+                RaisePropertyChanged("ProxyButtonText");
+            }
+        }
+        
+
+        public void StartSensor()
+        {
+            pollingThread = new ProximityReaderThread();
+            pollingThread.Start();
+
+            //initializes the specified task
+            Thread uiThread = new Thread(UIThreadLogic);
+            uiThread.Start();
+
+            //Thread fftThread = new Thread(new Par
+        }
+
+        int proxGraphCounter = 0;
+        private void UIThreadLogic()
+        {
+            while (true)
+            {
+                    double proximity = pollingThread.MostRecentProximity();
+                    if (proximity > 24000)
+                    {
+                        //update the graph
+                        lock (this.Model.SyncRoot)
+                        {
+                            MainSeries.Points.Add(new DataPoint(proxGraphCounter, proximity));
+                            proxGraphCounter++;
+                            if (proxGraphCounter > FFTSampleSize)
+                            {
+                                MainSeries.Points.RemoveAt(0);
+                                this.Model.Axes[0].Minimum = MainSeries.Points[0].X;
+                            }
+                        }
+                        this.Model.InvalidatePlot(true);
+                    }
+            }
+        }
+
+        public int FFTSampleSize { get; set; }
+
+        private void FFT()
+        {
+            var vector = pollingThread.SegmentOfProximities(FFTSampleSize);
+            //probably unnessecary
+            int vectorLength = vector.Count;
+            Exocortex.DSP.ComplexF[] complexData = new Exocortex.DSP.ComplexF[vectorLength];
+
+            for (int i = 0; i < vectorLength; ++i)
+            {
+                complexData[i].Re = vector.ElementAt(i); // Add your real part here
+                //    complexData[i].Im = 2; // Add your imaginary part here
+            }
+
+            Exocortex.DSP.Fourier.FFT(complexData, Exocortex.DSP.FourierDirection.Forward);
+        }
+    }
+}
---
+++ LDC1000ProximityPlot/ProximityReaderThread.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO.Ports;
+using System.Collections.Concurrent;
+
+namespace LDC1000ProximityPlot
+{
+    class ProximityReaderThread
+    {
+        private Thread t;
+        private SerialPort evm;
+        private int dataLength;
+        private int readByteCounter = 0;
+        private byte[] data = new byte[4];
+        private int fftBinSize = 1024;
+
+        //stores proximity data
+        private float[] dataProx = new float[1024000];
+
+        public double MostRecentProximity()
+        {
+            return dataProx[DataCounter];
+        }
+
+        public ArraySegment<float> SegmentOfProximities(int numSamples)
+        {
+            try
+            {
+                return new ArraySegment<float>(dataProx, DataCounter - fftBinSize, fftBinSize);
+            }
+            catch (Exception ex)
+            {
+                //return the first little guy instead
+                return new ArraySegment<float>(dataProx, 0, fftBinSize);
+            }
+        }
+        public int DataCounter { get; set; }
+
+        private ConcurrentQueue<double> proxQueue;
+        public void Dequeue(out double proximity)
+        {
+            proxQueue.TryDequeue(out proximity);
+        }
+
+        public ProximityReaderThread()
+        {
+            evm = new SerialPort("COM12", 115200); 
+            t = new Thread(PollProximity);
+            DataCounter = 0;
+        }
+
+        public void Start() 
+        {
+            evm.Open();
+            t.Priority = ThreadPriority.AboveNormal;
+            t.Start(); 
+        }
+        public void Stop() 
+        {
+            evm.Close();
+            t.Abort(); 
+        }
+
+        // note: this event is fired in the background thread
+        //public event EventHandler<DataEventArgs> DataReceived;
+
+        private bool closed = false;
+        public void Close() { closed = true; }
+
+        private void PollProximity()
+        {
+            while (true)
+            {
+                try
+                {
+                    evm.Write("1");
+                    dataLength = evm.BytesToRead;
+
+                    while (evm.BytesToRead > 0)
+                    {
+                        if (readByteCounter == dataLength) break;
+                        switch (readByteCounter)
+                        {
+                            case 2:
+                                data[0] = (byte)evm.ReadByte();
+                                break;
+                            case 3:
+                                data[1] = (byte)evm.ReadByte();
+                                break;
+                            case 4:
+                                data[2] = (byte)evm.ReadByte();
+                                break;
+                            case 5:
+                                data[3] = (byte)evm.ReadByte();
+                                break;
+                            default:
+                                evm.ReadByte();
+                                break;
+                        }
+                        readByteCounter++;
+                    }
+                    readByteCounter = 0;
+
+                    string hexFromASCII = System.Text.Encoding.ASCII.GetString(data);
+                    if (DataCounter >= dataProx.Length) DataCounter = 0;
+                    dataProx[DataCounter] = (float)int.Parse(hexFromASCII, System.Globalization.NumberStyles.AllowHexSpecifier);
+                    DataCounter++;
+
+                    //proxQueue.Enqueue(double.Parse(hexFromASCII, System.Globalization.NumberStyles.AllowHexSpecifier));
+                }
+                catch (Exception ex)
+                {
+                    //do nothing...yet
+                }
+            }
+        }
+    }
+}
